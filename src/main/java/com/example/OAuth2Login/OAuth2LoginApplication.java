110880f86eb498fa027170b2bfc114e61d626a26
shehap <shehap@gmail.com>
2023-09-26 03:36:52 +0300

    init commit

---
+++ src/main/java/com/example/OAuth2Login/OAuth2LoginApplication.java
@@ -0,0 +1,12 @@
+package com.example.OAuth2Login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2LoginApplication.class, args);
+	}
+}
